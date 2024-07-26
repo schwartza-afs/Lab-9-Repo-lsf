07528e96dd2856c360c9f33c8d82909c7f8aaae0
LouieSF <lsf2004impo@gmail.com>
2024-07-25 20:39:28 -0400

    added encode function

---
+++ lsf_Lab9.py
@@ -0,0 +1,17 @@
+
+def encode(password):
+    encoded = ""
+    for i in password:
+        if i == "7":
+            num = 0
+        elif i == "8":
+            num = 1
+        elif i == "9":
+            num = 2
+        else:
+            num = int(i)
+            num += 3
+        encoded += str(num)
+    return encoded
+
+print(encode(input("Enter a password to encode: ")))
